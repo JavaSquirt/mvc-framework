fffc24ac8c5b2db675645f3e63c227d08d76b0ad
JavaSquirt <c58.krypt0n@gmail.com>
2019-02-10 15:39:10 +0300

    add classes Router & Db

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 
-echo 'zdarova';
+require 'application/lib/Dev.php';
+
+use application\core\Router;
+use application\lib\Db;
+
+spl_autoload_register(function ($class) {
+    $path = str_replace('\\', '/', $class.'.php');
+    if (file_exists($path)) {
+    	require $path;
+    }
+});
+
+
+$router = new Router;
+$router = new Db;
